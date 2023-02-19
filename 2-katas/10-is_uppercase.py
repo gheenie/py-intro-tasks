c701c1291d8bbeb3d0accaf085fb60b2292bae97
Patrik <polsson.development@gmail.com>
2023-02-19 17:05:17 +0000

    Update 10-is_uppercase.py
    The function description doesn't match the test cases.  Changed description

--- 2-katas/10-is_uppercase.py
+++ 2-katas/10-is_uppercase.py
@@ -1,4 +1,4 @@
-# Create the function is_uppercase which returns a boolean idicating whether each character is uppercase in the passed string
+# Create the function is_uppercase which returns a boolean idicating whether any character is uppercase in the passed string
 # eg. is_uppercase('BANANA') => True
 #     is_uppercase('banana') => False
 #     is_uppercase('banAna') => True
